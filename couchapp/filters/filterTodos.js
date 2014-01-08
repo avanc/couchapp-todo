bfba3b5abd5627769cada51d20f87c17c4b9207e
Sven Klomp <mail@klomp.eu>
2014-01-08 20:59:38 +0100

    Changed wrong filter for replication

--- couchapp/filters/filterTodos.js
+++ couchapp/filters/filterTodos.js
@@ -2,7 +2,7 @@ function(doc, req) {
     if (doc.type && doc.type == 'todo') {
         return true;
     }
-    else if (doc.id == '_design/todo') {
+    else if (doc._id == '_design/todo') {
         return true;
     }
     else
