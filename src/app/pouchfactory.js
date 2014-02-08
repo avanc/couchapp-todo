1d8161e0e8dadb14c4064d0589a30eda98d94a32
Sven Klomp <mail@klomp.eu>
2014-02-08 13:05:55 +0100

    Add notification of replication activity
    Not all notification work without problems due to #28.

--- src/app/pouchfactory.js
+++ src/app/pouchfactory.js
@@ -2,7 +2,7 @@ require('./angular-cornerpouch.js');
 var helpers = require('./helpers.js');
 
 angular.module('PouchDB', ['ng', 'CornerCouch']).
-factory('pouchdb', ['$location', 'cornercouch',  function($location, cornercouch) {
+factory('pouchdb', ['$rootScope', '$location', '$timeout', 'cornercouch',  function($scope, $location, $timeout, cornercouch) {
     var uri = helpers.parseUri($location.absUrl());
     var remoteDatabase = uri.protocol + '//' + uri.host + '/' + uri.database + '/';
     var database = cornercouch().getDB(uri.database);
@@ -11,44 +11,55 @@ factory('pouchdb', ['$location', 'cornercouch',  function($location, cornercouch
     var replicationFrom;
     var obj = {replicating :false};
     
-    var onComplete1 = function() {
-            //alert("onComplete1");
+    var onComplete = function(res, err) {
+        console.log("Canceled continous replication");
+        database.stopReplication();
     };
-    var onComplete2 = function() {
-            //alert("onComplete2");
+    
+    var onChange = function(res) {
+        $scope.$apply( function() {
+            console.log("Changes during replication");
+            obj.replicating = "active";
+            $timeout(function() {
+                if (obj.replicating!==false) {
+                    obj.replicating=true;
+                }
+            }, 2000);
+        });
+        
     };
-    var onChange1 = function() {
-            //alert("onChange1");
-    };
-    var onChange2 = function() {
-            //alert("onChange");
-    };
+    
     
     database.startReplication = function() {
         database.stopReplication();
         
         replicationTo = database.replicateTo(remoteDatabase, {
             continuous: true,
-            complete: onComplete1,
-            onChange: onChange1
+            complete: onComplete,
+            onChange: onChange
         });
     
         replicationFrom = database.replicateFrom(remoteDatabase, {
             continuous: true,
             filter: 'todo/filterTodos',
-            complete: onComplete2,
-            onChange: onChange2
+            complete: onComplete,
+            onChange: onChange
         });
+        
         obj.replicating = true;
     };
     
     database.stopReplication = function() {
         if (typeof(replicationTo) != "undefined") {
-            replicationTo.cancel();
+            if (!replicationTo.cancelled) {
+                replicationTo.cancel();
+            }
             replicationTo = undefined;
         }
         if (typeof(replicationFrom) != "undefined") {
-            replicationFrom.cancel();
+            if (!replicationFrom.cancelled) {
+                replicationFrom.cancel();
+            }
             replicationFrom = undefined;
         }
         obj.replicating = false;
